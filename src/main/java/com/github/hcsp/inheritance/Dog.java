2c4d59a6658c3880815f0c7b7eaf8d14f984d519
buuoltwo <bullnick20@gmail.com>
2024-12-06 10:12:57 +0800

    通过Test2: ClassInspector.assertNoDeclaredFields(Cat.class, Dog.class, Rat.class);

--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -1,19 +1,19 @@
 package com.github.hcsp.inheritance;
 
 public class Dog extends Animal{
-    private String name;
+//    private String name;
 
     public Dog(String name) {
         super(name);
-        this.name = name;
+//        this.name = name;
 //        super(name);    //Call to 'super()' must be first statement in constructor body
     }
 
     public void sayMyName() {
-        System.out.println("我的名字是" + name);
+        System.out.println("我的名字是" + this.getName());
     }
 
     public void wang() {
-        System.out.println("汪" + name);
+        System.out.println("汪" + super.getName());
     }
 }
