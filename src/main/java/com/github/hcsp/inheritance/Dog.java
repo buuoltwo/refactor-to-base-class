041d88fe795f7a58c71ab91a93b26c1df9ad7fae
buuoltwo <bullnick20@gmail.com>
2024-12-06 10:14:16 +0800

    通过Test3: Assertions.assertEquals(1, Dog.class.getDeclaredMethods().length);

--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -9,9 +9,9 @@ public class Dog extends Animal{
 //        super(name);    //Call to 'super()' must be first statement in constructor body
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + this.getName());
-    }
+//    public void sayMyName() {
+//        System.out.println("我的名字是" + this.getName());
+//    }
 
     public void wang() {
         System.out.println("汪" + super.getName());
